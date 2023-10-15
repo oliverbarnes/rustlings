1972377f9c5ded1b0877353ca0a7045deaa3a5eb
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2023-10-16 00:19:52 +0100

    About to start with struct exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty:i32) -> i32 {
+    let mut price:i32 = 2;
+
+    if qty > 40 {
+        price = 1;
+    }
+
+    qty * price
+}
 
 // Don't modify this function!
 #[test]
